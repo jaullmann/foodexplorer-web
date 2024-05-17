436f161b1ea6fbd6222b8ffc07d1fdd019a86c26
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-05-17 17:45:09 -0300

    Beginning components development - 17-05-2024 17h45

---
+++ src/styles/global.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+import { DEVICE_BREAKPOINTS } from "./deviceBreakpoints";
+
+export default createGlobalStyle`
+    * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+
+    :root {
+        font-size: 16px;
+
+        @media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+            font-size: 12px;
+        }
+    }
+
+    body {
+        background-color: ${({ theme }) => theme.COLORS.DARK_100};
+        color: ${({ theme }) => theme.COLORS.LIGHT_100}; 
+        
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, button, textarea {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 1rem;
+        outline: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.7);
+    }
+`
