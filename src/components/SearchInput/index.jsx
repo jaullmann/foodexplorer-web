c8a53035a9fe3be072b418b6f46f3ae882d98835
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-07-25 15:30:53 -0300

    SearchInput working - solution 1

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -1,18 +1,37 @@
+import { useState, useEffect } from "react";
 import { FiSearch } from "react-icons/fi";
 import { Container, Input } from "./styles";
+import { useNavigate } from "react-router-dom";
 
-export function SearchInput(){
+export function SearchInput() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setSearchQuery(value);
+  };
+
+  useEffect(() => {
+    if (searchQuery) {
+      const timer = setTimeout(() => {
+        navigate(`/search/${searchQuery}`);
+      }, 300);
+      return () => clearTimeout(timer);
+    }
+  }, [searchQuery, navigate]);
+
   return (
-    <Container id="search">      
-      <Input         
+    <Container id="search">
+      <Input
         autoFocus
-        value={undefined}
-        onChange={undefined}
+        value={searchQuery}
+        onChange={handleInputChange}
         placeholder="Busque por pratos ou ingredientes"
       />
       <button>
-        <FiSearch/>
-      </button>       
-    </Container>    
-  )
+        <FiSearch />
+      </button>
+    </Container>
+  );
 }
