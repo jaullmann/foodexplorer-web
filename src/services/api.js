cef8db2113bce20130f73382002a6c19f435bf21
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-08-31 18:49:11 -0300

    Updated api.js to use the current backend online application.

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: "https://foodexplorer-api-x65p.onrender.com",
-  // baseURL: "http://localhost:4000",  
   withCredentials: true
 });
