e76e4679bcb022ab40e5340d6188fbc42c170fee
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-09-02 13:52:09 -0300

    Reverted last commit

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -8,8 +8,8 @@ export const Section = styled.footer`
   width: 100%;
   height: 4.81rem;
 
-  position: absolute;
-  bottom: 0;  
+  /* position: absolute;
+  bottom: 0;   */
 
   background-color: ${({ theme }) => theme.COLORS.DARK_700};
 
