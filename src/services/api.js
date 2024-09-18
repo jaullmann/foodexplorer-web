2abcb30d1670d1432fd0a089ed027b658ee8265e
Jorge Alencar Ullmann <jaullmann@gmail.com>
2024-09-18 17:02:27 -0300

    api.js updated to connect with online REST API server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://foodexplorer-api-x65p.onrender.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://foodexplorer-api-x65p.onrender.com",
+  // baseURL: "http://localhost:4000",
   withCredentials: true
 });
